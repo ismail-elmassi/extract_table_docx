5148a9b6f05fbf72d211bdac9636ec34a4a60dd3
ismail-elmassi <ismail.elmassi@gmail.com>
2022-11-03 22:38:38 +0100

    correct test

--- tests/test_check_extracted_text.py
+++ tests/test_check_extracted_text.py
@@ -4,7 +4,7 @@ from src.extract_questions_from_word import do_table_extraction
 
 def test_extract_table_questions():
 	extracted_questions= do_table_extraction(os.path.abspath("tests/resources/test_file.docx"))
-	assert extracted_questions==["What’s your name?","How old are youu?"]
+	assert extracted_questions==["What’s your name?","How old are you?"]
 
 
 
