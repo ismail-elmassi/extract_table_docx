41e1678ff1f4a819979c20b7f2826d8abaa406e9
ismail-elmassi <ismail.elmassi@gmail.com>
2022-11-03 23:39:46 +0100

    pylint correction

--- src/extract_questions_from_word.py
+++ src/extract_questions_from_word.py
@@ -1,11 +1,13 @@
+"""
+This is  extract questions from word file
+"""
 import pandas as pd
 from docx.api import Document
-import sys
-import os
 
 UPLOADS_FOLDER = "uploads"
 
 def do_extraction(filename):
+    """ This is  do extraction function """
     table_questions = do_table_extraction(filename)
     resp = {}
     if table_questions:
@@ -13,21 +15,21 @@ def do_extraction(filename):
     return resp
 
 def do_table_extraction(filename):
+    """ This is  do table extraction function """
     document = Document(filename)
     output = []
-    
     for table in document.tables:
-    	data = []
-    	keys = None
-    	for i, row in enumerate(table.rows):
-    		text = (cell.text for cell in row.cells)
-    		if i == 0:
-    			keys = tuple(text)
-    			continue
-    		row_data = dict(zip(keys, text))
-    		data.append(row_data)
-    	df = pd.DataFrame(data)
-    	output = df.T.reset_index().values.tolist()[0]
-    	output = list(filter(None, output))
-    	print(output)
+        data = []
+        keys = None
+        for i, row in enumerate(table.rows):
+            text = (cell.text for cell in row.cells)
+            if i == 0:
+                keys = tuple(text)
+                continue
+            row_data = dict(zip(keys, text))
+            data.append(row_data)
+        df_data = pd.DataFrame(data)
+        output = df_data.T.reset_index().values.tolist()[0]
+        output = list(filter(None, output))
+        print(output)
     return output
